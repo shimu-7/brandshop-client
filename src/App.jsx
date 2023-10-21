8f40af34a81d473ed8766b648be2de299f3b29d2
shimu-7 <shamima19shimu@gmail.com>
2023-10-22 03:17:32 +0600

    brand json file created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 
+import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
-  
+  const [brands, setBrands] = useState([])
+
+  useEffect(() => {
+    fetch('brands.json')
+      .then(res => res.json())
+      .then(data => setBrands(data))
+  }, [])
+
 
   return (
     <>
-     
+
       <h1>Welcome to Brand Shop</h1>
-      
+      <div className='grid grid-cols-3 gap-3'>
+        {
+          brands.map(brand => <div key={brand.id}><img src={brand.image}></img></div>)
+        }
+      </div>
+
     </>
   )
 }
